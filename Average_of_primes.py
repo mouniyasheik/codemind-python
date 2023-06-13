f0fc448e99bc45ac22b83da1b0b929d5fc2ed740
22p31a0534 <119798543+mouniyasheik@users.noreply.github.com>
2023-06-13 21:52:38 +0530

    def fun(n):     for i in range(2,n//2+1):         if n%i==0:             return 0     return 1 n=int(input()) s=0 c=0 a=list(map(int,input().split())) for i in a:     if i==1:         continue     else:         if fun(i)==1:             s=s+i             c=c+1 print(format(s/c,".2f"))

---
+++ Average_of_primes.py
@@ -0,0 +1,17 @@
+def fun(n):
+    for i in range(2,n//2+1):
+        if n%i==0:
+            return 0
+    return 1
+n=int(input())
+s=0
+c=0
+a=list(map(int,input().split()))
+for i in a:
+    if i==1:
+        continue
+    else:
+        if fun(i)==1:
+            s=s+i
+            c=c+1
+print(format(s/c,".2f"))
