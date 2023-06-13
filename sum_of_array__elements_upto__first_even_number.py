29803ab3199f58803eb90eaf4fb194a26f72e27e
22p31a0534 <119798543+mouniyasheik@users.noreply.github.com>
2023-06-13 21:27:34 +0530

    n=int(input()) s=m=0 c=[] l=list(map(int,input().split()))
    for i  in  range(len(l)):
        if  l[i]%2!=0:
            c.append(l[i])
            s=s+l[i]
        else:
            m+=1
            break
    print(s)

---
+++ sum_of_array__elements_upto__first_even_number.py
@@ -0,0 +1,13 @@
+n=int(input())
+s=m=0
+c=[]
+l=list(map(int,input().split()))
+
+for i  in  range(len(l)):
+    if  l[i]%2!=0:
+        c.append(l[i])
+        s=s+l[i]
+    else:
+        m+=1
+        break
+print(s)
