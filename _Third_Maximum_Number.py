998bd3d5ff8f95ffde5ae92ce783b3969b45546c
22p31a0534 <119798543+mouniyasheik@users.noreply.github.com>
2023-07-07 15:33:26 +0530

    n=int(input()) l=list(map(int,input().split())) p=[] for  i in l:     if i not in p:         p.append(i) p.sort() if len(l)>=3:     print(p[-3]) elif len(l)<3:     print(max(l))

--- _Third_Maximum_Number.py
+++ _Third_Maximum_Number.py
@@ -1,12 +1,11 @@
 n=int(input())
 l=list(map(int,input().split()))
-s=set(l)
 p=[]
 for  i in l:
     if i not in p:
         p.append(i)
 p.sort()
-if len(s)>=3:
+if len(l)>=3:
     print(p[-3])
 elif len(l)<3:
     print(max(l))
